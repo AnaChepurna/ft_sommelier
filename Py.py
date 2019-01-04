f71e5be6201044af6ecf234ce4fb2c27d3bf2eb1
AnaChepurna <ch.nastasie@gmail.com>
2019-01-04 22:14:17 +0200

    plot decision done

--- Py.py
+++ Py.py
@@ -30,6 +30,8 @@ def plot_scatter_matrix(wine_data, good_threshold, bad_threshold, save_plot=Fals
 
 path = 'reds.csv'
 wine_data = pandas.read_csv(path, sep=';')
+
+
 # plot_scatter_matrix(wine_data, 8, 3)
 
 
@@ -37,7 +39,10 @@ class Perceptron:
     def __init__(self, num, lr=0.01):
         self.lr = lr
         self.weights = [random.uniform(-1, 1) for i in range(num)]
+        # self.weights[0] = 10.26190938115157
+        # self.weights[1] = -6.7332574104080807
         self.bias = random.uniform(-1, 1)
+        # self.bias = 10.125182027822518
 
     def _activation(self, x):
         return 1 if x >= 1 else 0
@@ -57,7 +62,7 @@ class Perceptron:
             # e = y*(1 - y)*(data_output[i] - y)
             # self.weights = list(map(lambda x : x[1] - e * self.lr * x[0], zip(data_input.iloc[i], self.weights)))
             # self.bias -= e * self.lr
-            error += abs(e)
+            error += abs(e / self.lr)
         return error
 
     def train(self, data_input, data_output, epoh_num=0):
@@ -82,4 +87,75 @@ data_output = list(map(lambda x: float(x >= 8), selected['quality']))
 
 perceptron = Perceptron(2)
 log = perceptron.train(data_input, data_output)
-print(log)
+
+
+# print(log)
+
+
+def plot_errors_epoh(ax, performance):
+    ax.set_title("Classification Errors vs. Epohs")
+    ax.set_xlabel('epoch')
+    ax.set_ylabel('errors')
+    ax.plot([elem[0] for elem in performance], [elem[1] for elem in performance])
+
+
+def plot_decision_boundary(ax, wine_data, good_thresh, bad_thresh, performance, epoh):
+    ax.set_title("Decision Boundary on epoh: {}".format(epoh))
+    ax.set_xlabel('alcohol')
+    ax.set_ylabel('pH')
+    y_min = wine_data['pH'].min()
+    y_max = wine_data['pH'].max()
+    x_min = wine_data['alcohol'].min()
+    x_max = wine_data['alcohol'].max()
+    w1, w2 = performance[epoh][2]
+    b = performance[epoh][3]
+    y = [y_min, y_max]
+    x = list(map(lambda i: (i * w1 + b - 1) / -w2, y))
+    ax.plot(x, y, 'b--', label='decision boundary')
+    x_max = x_max if x_max > x[1] else x[1]
+    x_min = x_min if x_min < x[0] else x[0]
+    x += [x_max, x_max]
+    y += [y_max, y_min]
+    ax.fill_between(x, y, y_min, color='#ccffcc')
+    x[2], x[3] = x_min, x_min
+    y[2], y[3] = y_max, y_min
+    ax.fill_between(x, y, y_min, color='#ffcccc')
+    goods = wine_data[(wine_data['quality'] >= good_thresh)]
+    bads = wine_data[(wine_data['quality'] <= bad_thresh)]
+    ax.scatter(bads['alcohol'], bads['pH'], c=['r'], label='bad wines')
+    ax.scatter(goods['alcohol'], goods['pH'], c=['g'], label='good wines')
+    ax.margins(x=0, y=0)
+    ax.legend(bbox_to_anchor=(1.05, 1), loc=2)
+
+
+def plot_performance(performance, wine_data, good_thresh, bad_thresh, epoh=-1, save_plot=False):
+    if epoh < 0 | len(performance) > epoh:
+        epoh = len(performance) - 1
+    f, axs = plot.subplots(ncols=2, figsize=(15, 7))
+    plot_errors_epoh(axs[0], performance)
+    plot_decision_boundary(axs[1], wine_data, good_thresh, bad_thresh, performance, epoh)
+    if save_plot:
+        f.savefig('./plot_performance.png')
+    else:
+        f.show()
+
+
+plot_performance(log, wine_data, 8, 3)
+
+min_pH = wine_data["pH"].min()
+max_pH = wine_data["pH"].max()
+min_alcohol = wine_data["alcohol"].min()
+max_alcohol = wine_data["alcohol"].max()
+
+wine_data['alcohol'] = list(map(lambda w: (w - min_alcohol) / (max_alcohol - min_alcohol), wine_data["alcohol"]))
+wine_data['pH'] = list(map(lambda w: (w - min_pH) / (max_pH - min_pH), wine_data["pH"]))
+selected = wine_data[(wine_data['quality'] >= 8) | (wine_data['quality'] <= 3)]
+selected = selected.reset_index(drop=True)
+data_input_norm = selected[['pH', 'alcohol']]
+# data_input_norm = {"pH" : data_input_norm_p, "alcohol" : data_input_norm_a}
+
+# print(data_input_norm)
+
+# perceptron = Perceptron(2)
+# log = perceptron.train(data_input_norm, data_output)
+# plot_performance(log, wine_data, 8, 3)
